06bf6c6338676b4659c203b301aaec7dc0557725
antonyoni <antony@reduc.tech>
2021-06-28 11:19:14 +0100

    Remove reference to SCLSettings

--- Sql/ConnectorInjection.cs
+++ Sql/ConnectorInjection.cs
@@ -1,17 +1,17 @@
 ﻿using System.Collections.Generic;
 using CSharpFunctionalExtensions;
-using Reductech.EDR.Core;
 using Reductech.EDR.Core.Connectors;
 using Reductech.EDR.Core.Internal.Errors;
 
 namespace Reductech.EDR.Connectors.Sql
 {
 
+/// <inheritdoc />
 public sealed class ConnectorInjection : IConnectorInjection
 {
     /// <inheritdoc />
     public Result<IReadOnlyCollection<(string Name, object Context)>, IErrorBuilder>
-        TryGetInjectedContexts(SCLSettings settings)
+        TryGetInjectedContexts()
     {
         var list = new List<(string Name, object Context)>()
         {
